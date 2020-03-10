d3d4bef6d7f544da902ab74fc2b7ae9f062c8f3b
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-10 02:23:54 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ApplicationRecord
  validates :title, :artist_name, :release_year, presence: true
  validates :title, uniqueness: true
  validates :released, inclusion: { in: [true, false] }
- validates :release_year, 
+ #validates :release_year must not be less than or equal to the current year
 end

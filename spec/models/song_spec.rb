d1ec5dae897ab6bd38c6d02fac1f001fd1964792
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-10 03:00:05 +0000

    Automatically backed up by Learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  
+  before do
+    @song = Song.create(title: "My Song", )
+  end
 end

1ff88672a0289abbb0cba4e8c0570de00378f67a
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-10 03:01:09 +0000

    Automatically backed up by Learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Song, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   before do
-    @song = Song.create(title: "My Song", )
+    @song = Song.create(title: "My Song", released: true, release_year: 2010, artist_name: "Alicia Keys", genre: "R&B")
   end
 end

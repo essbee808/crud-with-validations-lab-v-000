aa801b3fc521c02498e26ee2c4b51e110f0baa7a
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-10 02:22:31 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
  validates :title, :artist_name, presence: true
  validates :title, uniqueness: true
- validates_inclusion_of :released, :in => %w()
+ validates :released, inclusion: { in: [true, false] }
 end
